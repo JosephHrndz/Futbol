b83c57cf3285b7c20b81ffb5a08f2f9a6e06f2f2
JosephHrndz <ah22003@esfe.agape.edu.sv>
2023-08-28 08:07:00 -0600

    Subir Cambios

--- ProyectoFutbol.Web/src/java/ProyectoFutbol/Web/Utils/IAuthorize.java
+++ ProyectoFutbol.Web/src/java/ProyectoFutbol/Web/Utils/IAuthorize.java
@@ -8,6 +8,12 @@ package ProyectoFutbol.Web.Utils;
  *
  * @author JOSEPH
  */
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+/*
+Esta interfaz la utlizaramos para usar una expresion lambda, 
+al momento de autorizar un usuario al servlet
+*/
 public interface IAuthorize {
-    
+    void authorize() throws ServletException, IOException;
 }
---
+++ ProyectoFutbol.Web/web/Views/Shared/footerBody.jsp
@@ -0,0 +1,14 @@
+<%-- 
+    Document   : footerBody
+    Created on : 27 ago 2023, 14:58:27
+    Author     : JOSEPH
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<script>
+    $(document).ready(function () {
+        $('.sidenav').sidenav();
+        $('select').formSelect();
+         M.updateTextFields();
+    });
+</script>
