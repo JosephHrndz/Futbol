ddc16f3a99594a55f4c25e8f96f1e11bbf850127
edenilson33 <edenilson33@outlook.es>
2023-08-29 12:02:31 -0300

    Listo SerieA

--- ProyectoFutbol.Web/src/java/ProyectoFutbol/Web/Controllers/SerieaServlet.java
+++ ProyectoFutbol.Web/src/java/ProyectoFutbol/Web/Controllers/SerieaServlet.java
@@ -12,11 +12,16 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.util.ArrayList;
+import proyectofutbol.accesoadatos.SerieADAL;
+import proyectofutbol.entidaddenegocio.SerieA;
+import ProyectoFutbol.Web.Utils.*;
+
 /**
  *
  * @author MINEDUCYT
  */
-@WebServlet(name = "SerieaServlet", urlPatterns = {"/SerieaServlet"})
+@WebServlet(name = "SerieAServlet", urlPatterns = {"/SerieA"})
 public class SerieaServlet extends HttpServlet {
 
     /**
@@ -28,23 +33,216 @@ public class SerieaServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+      private SerieA obtenerSerieA(HttpServletRequest request)
+    {
+        String accion = Utilidad.getParameter(request, "accion", "index");
+        SerieA seriea = new SerieA ();
+        if(accion.equals("create") == false)
+        {
+            //Obtiene el parametro de Id del request y asigna el valor a la propiedad 
+            //Id de la instancia
+            seriea.setId(Integer.parseInt(Utilidad.getParameter(request, "id",
+                    "0")));
+        }
+        seriea.setEquipo(Utilidad.getParameter(request, "Equipo", ""));
+        if(accion.equals("index"))
+        {
+            seriea.setTop_aux(Integer.parseInt(Utilidad.getParameter(request, 
+                    "top_aux", "10")));
+            seriea.setTop_aux(seriea.getTop_aux() == 0 ? Integer.MAX_VALUE: seriea.getTop_aux());
+        }
+         if(accion.equals("index"))
+        {
+            //Obtiene el parametro de Id del request y asigna el valor a la propiedad 
+            //Id de la instancia
+            seriea.setIdLigas(Integer.parseInt(Utilidad.getParameter(request, "idLigas",
+                    "0")));
+        }
+          if(accion.equals("index"))
+        {
+            //Obtiene el parametro de Id del request y asigna el valor a la propiedad 
+            //Id de la instancia
+            seriea.setPosicion(Integer.parseInt(Utilidad.getParameter(request, "posicion",
+                    "0")));
+        }
+          
+       if(accion.equals("index"))
+        {
+            //Obtiene el parametro de Id del request y asigna el valor a la propiedad 
+            //Id de la instancia
+            seriea.setPuntos(Integer.parseInt(Utilidad.getParameter(request, "puntos",
+                    "0")));
+        }
+       if(accion.equals("index"))
+        {
+            seriea.setTop_aux(Integer.parseInt(Utilidad.getParameter(request, 
+                    "top_aux", "10")));
+            seriea.setTop_aux(seriea.getTop_aux() == 0 ? Integer.MAX_VALUE: seriea.getTop_aux());
+        }
+          
+    
+        return seriea;
+    }
+     
+     
+        protected void doGetRequestIndex(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SerieaServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SerieaServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        try
+        {
+            SerieA seriea = new SerieA();
+            seriea.setTop_aux(10);
+            ArrayList<SerieA> series = SerieADAL.buscar(seriea);
+            request.setAttribute("seriea", seriea);
+            request.setAttribute("top_aux", seriea.getTop_aux());
+            request.getRequestDispatcher("Views/seriea/index.jsp")
+                    .forward(request, response);
+        }
+        catch(Exception ex)
+        {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+        
+            protected void doPostRequestIndex(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try
+        {
+            SerieA seriea = obtenerSerieA(request);
+            ArrayList<SerieA> series  = SerieADAL.buscar(seriea);
+            request.setAttribute("seriea", seriea);
+            request.setAttribute("top_aux",seriea.getTop_aux());
+            request.getRequestDispatcher("Views/SerieA/index.jsp")
+                    .forward(request, response);
+        }
+        catch(Exception ex)
+        {
+            Utilidad.enviarError(ex.getMessage(), request, response);
         }
     }
 
+    protected void doGetRequestCreate(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("Views/SerieA/create.jsp")
+                .forward(request, response);
+    }
+    
+    
+       protected void doPostRequestCreate(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try
+        {
+            SerieA  seriea = obtenerSerieA(request);
+            int result = SerieADAL.crear(seriea);
+            if(result != 0)
+            {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            }
+            else
+            {
+                Utilidad.enviarError("Error al Guardar el Regisgtro", request, response);
+            }
+
+        }
+        catch(Exception ex)
+        {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+       
+       
+    protected void requestObtenerPorId(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try
+        {
+            SerieA seriea = obtenerSerieA(request);
+            SerieA seriea_result = SerieADAL.obtenerPorId(seriea);
+            if(seriea_result.getId() > 0)
+            {
+                request.setAttribute("laLiga", seriea_result);
+            }
+            else
+            {
+                Utilidad.enviarError("El id: " + seriea.getId() + " no existe en la tabla laLiga", 
+                        request, response);
+            }
+        }
+        catch(Exception ex)
+        {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    
+      protected void doGetRequestEdit(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            requestObtenerPorId(request, response);
+            request.getRequestDispatcher("Views/SerieA/edit.jsp")
+                    .forward(request, response);
+    }
+      
+        protected void doPostRequestEdit(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try
+        {
+             SerieA seriea = obtenerSerieA(request);
+            int result = SerieADAL.modificar(seriea);
+            if(result != 0)
+            {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            }
+            else
+            {
+                Utilidad.enviarError("Error al Guardar el Regisgtro", request, response);
+            }
+
+        }
+        catch(Exception ex)
+        {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+        
+        
+           protected void doGetRequestDetails(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            requestObtenerPorId(request, response);
+            request.getRequestDispatcher("Views/SerieA/details.jsp")
+                    .forward(request, response);
+    }
+    
+    protected void doGetRequestDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            requestObtenerPorId(request, response);
+            request.getRequestDispatcher("Views/SerieA/delete.jsp")
+                    .forward(request, response);
+    }
+    
+    
+      protected void doPostRequestDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try
+        {
+            SerieA  seriea = obtenerSerieA (request);
+            int result = SerieADAL.eliminar(seriea);
+            if(result != 0)
+            {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            }
+            else
+            {
+                Utilidad.enviarError("Error al Guardar el Regisgtro", request, response);
+            }
+
+        }
+        catch(Exception ex)
+        {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -57,8 +255,39 @@ public class SerieaServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        SessionUser.authorize(request, response, () -> {
+            String accion = Utilidad.getParameter(request, 
+                    "accion", "index");
+            switch(accion)
+            {
+                case "index":
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+                    break;
+                case "create":
+                    request.setAttribute("accion", accion);
+                    doGetRequestCreate(request, response);
+                    break;
+                case "edit":
+                    request.setAttribute("accion", accion);
+                    doGetRequestEdit(request, response);
+                    break;
+                case "delete":
+                    request.setAttribute("accion", accion);
+                    doGetRequestDelete(request, response);
+                    break;
+                case "details":
+                    request.setAttribute("accion", accion);
+                    doGetRequestDetails(request, response);
+                    break;
+                default:
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+                    break;
+            }
+        });
     }
+
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -69,19 +298,34 @@ public class SerieaServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+ protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+        //SessionUser.authorize(request, response, () -> {
+            String accion = Utilidad.getParameter(request, 
+                    "accion", "index");
+            switch(accion)
+            {
+                case "index":
+                    request.setAttribute("accion", accion);
+                    doPostRequestIndex(request, response);
+                    break;
+                case "create":
+                    request.setAttribute("accion", accion);
+                    doPostRequestCreate(request, response);
+                    break;
+                case "edit":
+                    request.setAttribute("accion", accion);
+                    doPostRequestEdit(request, response);
+                    break;
+                case "delete":
+                    request.setAttribute("accion", accion);
+                    doPostRequestDelete(request, response);
+                    break;
+                default:
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+                    break;
+            }
+        //});
+ }
 }
