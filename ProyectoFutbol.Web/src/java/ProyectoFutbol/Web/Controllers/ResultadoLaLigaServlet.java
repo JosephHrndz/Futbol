c353267b06f499cea0b79c7f709fd69fb1213d05
ayalaalberto <ma22002@esfe.agape.edu.sv>
2023-08-28 21:42:14 -0600

    actualizacicon

--- ProyectoFutbol.Web/src/java/ProyectoFutbol/Web/Controllers/ResultadoLaLigaServlet.java
+++ ProyectoFutbol.Web/src/java/ProyectoFutbol/Web/Controllers/ResultadoLaLigaServlet.java
@@ -12,11 +12,16 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.util.ArrayList;
+import proyectofutbol.accesoadatos.ResultadoLaLigaDAL;
+import proyectofutbol.entidaddenegocio.ResultadosLaLiga;
+import ProyectoFutbol.Web.Utils.*;
+
 /**
  *
  * @author MINEDUCYT
  */
-@WebServlet(name = "ResultadoLaLigaServlet", urlPatterns = {"/ResultadoLaLigaServlet"})
+@WebServlet(name = "ResultadoLaLigaServlet", urlPatterns = {"/ResultadoLaLiga"})
 public class ResultadoLaLigaServlet extends HttpServlet {
 
     /**
@@ -28,20 +33,156 @@ public class ResultadoLaLigaServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    private ResultadosLaLiga obtenerResultadosLaLiga(HttpServletRequest request) {
+        String accion = Utilidad.getParameter(request, "accion", "index");
+        ResultadosLaLiga resultadoslaliga = new ResultadosLaLiga();
+        if (accion.equals("create") == false) {
+            //Obtiene el parametro de Id del request y asigna el valor a la propiedad 
+            //Id de la instancia
+            resultadoslaliga.setId(Integer.parseInt(Utilidad.getParameter(request, "id",
+                    "0")));
+        }
+        resultadoslaliga.setEquipo1(Utilidad.getParameter(request, "Equipo1", ""));
+        if (accion.equals("index")) {
+            resultadoslaliga.setTop_aux(Integer.parseInt(Utilidad.getParameter(request,
+                    "top_aux", "10")));
+            resultadoslaliga.setTop_aux(resultadoslaliga.getTop_aux() == 0 ? Integer.MAX_VALUE : resultadoslaliga.getTop_aux());
+        }
+        resultadoslaliga.setResultadoDeportes(Utilidad.getParameter(request, "ResultadoDeportes", ""));
+        if (accion.equals("index")) {
+            resultadoslaliga.setTop_aux(Integer.parseInt(Utilidad.getParameter(request,
+                    "top_aux", "10")));
+            resultadoslaliga.setTop_aux(resultadoslaliga.getTop_aux() == 0 ? Integer.MAX_VALUE : resultadoslaliga.getTop_aux());
+        }
+        resultadoslaliga.setEquipo2(Utilidad.getParameter(request, "Equipo2", ""));
+        if (accion.equals("index")) {
+            resultadoslaliga.setTop_aux(Integer.parseInt(Utilidad.getParameter(request,
+                    "top_aux", "10")));
+            resultadoslaliga.setTop_aux(resultadoslaliga.getTop_aux() == 0 ? Integer.MAX_VALUE : resultadoslaliga.getTop_aux());
+        }
+        return resultadoslaliga;
+    }
+
+    protected void doGetRequestIndex(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ResultadoLaLigaServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ResultadoLaLigaServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        try {
+            ResultadosLaLiga resultadoslaliga = new ResultadosLaLiga();
+            resultadoslaliga.setTop_aux(10);
+            ArrayList<ResultadosLaLiga> resultadolaliga = ResultadoLaLigaDAL.buscar(resultadoslaliga);
+            request.setAttribute("resultadoslaliga", resultadoslaliga);
+            request.setAttribute("top_aux", resultadoslaliga.getTop_aux());
+            request.getRequestDispatcher("Views/ResultadosLaLiga/index.jsp")
+                    .forward(request, response);
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+    protected void doPostRequestIndex(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            ResultadosLaLiga resultadoslaliga = obtenerResultadosLaLiga(request);
+            ArrayList<ResultadosLaLiga> resultadolaliga = ResultadoLaLigaDAL.buscar(resultadoslaliga);
+            request.setAttribute("resultadoslaliga", resultadoslaliga);
+            request.setAttribute("top_aux", resultadoslaliga.getTop_aux());
+            request.getRequestDispatcher("Views/ResultadosLaLiga/index.jsp")
+                    .forward(request, response);
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+    protected void doGetRequestCreate(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("Views/ResultadosLaLiga/create.jsp")
+                .forward(request, response);
+    }
+
+    protected void doPostRequestCreate(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            ResultadosLaLiga resultadoslaliga = obtenerResultadosLaLiga(request);
+            int result = ResultadoLaLigaDAL.crear(resultadoslaliga);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("Error al Guardar el Regisgtro", request, response);
+            }
+
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+    protected void requestObtenerPorId(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            ResultadosLaLiga resultadoslaliga = obtenerResultadosLaLiga(request);
+            ResultadosLaLiga resultadoslaliga_result = ResultadoLaLigaDAL.obtenerPorId(resultadoslaliga);
+            if (resultadoslaliga_result.getId() > 0) {
+                request.setAttribute("resultadoslaliga", resultadoslaliga_result);
+            } else {
+                Utilidad.enviarError("El id: " + resultadoslaliga.getId() + " no existe en la tabla resultadoslaliga",
+                        request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+    protected void doGetRequestEdit(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/ResultadosLaLiga/edit.jsp")
+                .forward(request, response);
+    }
+
+    protected void doPostRequestEdit(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            ResultadosLaLiga resultadoslaliga = obtenerResultadosLaLiga(request);
+            int result = ResultadoLaLigaDAL.modificar(resultadoslaliga);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("Error al Guardar el Regisgtro", request, response);
+            }
+
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+    protected void doGetRequestDetails(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/ResultadosLaLiga/details.jsp")
+                .forward(request, response);
+    }
+
+    protected void doGetRequestDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/ResultadosLaLiga/delete.jsp")
+                .forward(request, response);
+    }
+
+    protected void doPostRequestDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            ResultadosLaLiga resultadoslaliga = obtenerResultadosLaLiga(request);
+            int result = ResultadoLaLigaDAL.eliminar(resultadoslaliga);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("Error al Guardar el Regisgtro", request, response);
+            }
+
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
         }
     }
 
@@ -57,7 +198,36 @@ public class ResultadoLaLigaServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        SessionUser.authorize(request, response, () -> {
+            String accion = Utilidad.getParameter(request,
+                    "accion", "index");
+            switch (accion) {
+                case "index":
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+                    break;
+                case "create":
+                    request.setAttribute("accion", accion);
+                    doGetRequestCreate(request, response);
+                    break;
+                case "edit":
+                    request.setAttribute("accion", accion);
+                    doGetRequestEdit(request, response);
+                    break;
+                case "delete":
+                    request.setAttribute("accion", accion);
+                    doGetRequestDelete(request, response);
+                    break;
+                case "details":
+                    request.setAttribute("accion", accion);
+                    doGetRequestDetails(request, response);
+                    break;
+                default:
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+                    break;
+            }
+        });
     }
 
     /**
@@ -71,17 +241,32 @@ public class ResultadoLaLigaServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //SessionUser.authorize(request, response, () -> {
+        String accion = Utilidad.getParameter(request,
+                "accion", "index");
+        switch (accion) {
+            case "index":
+                request.setAttribute("accion", accion);
+                doPostRequestIndex(request, response);
+                break;
+            case "create":
+                request.setAttribute("accion", accion);
+                doPostRequestCreate(request, response);
+                break;
+            case "edit":
+                request.setAttribute("accion", accion);
+                doPostRequestEdit(request, response);
+                break;
+            case "delete":
+                request.setAttribute("accion", accion);
+                doPostRequestDelete(request, response);
+                break;
+            default:
+                request.setAttribute("accion", accion);
+                doGetRequestIndex(request, response);
+                break;
+        }
+        //});
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
--- ProyectoFutbol.Web/src/java/ProyectoFutbol/Web/Controllers/ResultadoSerieaServlet.java
+++ ProyectoFutbol.Web/src/java/ProyectoFutbol/Web/Controllers/ResultadoSerieaServlet.java
@@ -13,16 +13,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.ArrayList;
-import proyectofutbol.accesoadatos.BundesLigaDAL;
-import proyectofutbol.entidaddenegocio.BundesLiga;
-import ProyectoFutbol.Web.utils.*;
 
+import proyectofutbol.accesoadatos.ResultadoSerieADAL;
+import proyectofutbol.entidaddenegocio.ResultadosSerieA;
+import ProyectoFutbol.Web.Utils.*;
 
 /**
  *
  * @author MINEDUCYT
  */
-@WebServlet(name = "ResultadoSerieaServlet", urlPatterns = {"/ResultadoSerieaServlet"})
+@WebServlet(name = "ResultadoSerieaServlet", urlPatterns = {"/ResultadoSerieA"})
 public class ResultadoSerieaServlet extends HttpServlet {
 
     /**
@@ -34,20 +34,157 @@ public class ResultadoSerieaServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    private ResultadosSerieA obtenerResultadosSerieA(HttpServletRequest request) {
+        String accion = Utilidad.getParameter(request, "accion", "index");
+        ResultadosSerieA resultadosseriea = new ResultadosSerieA();
+        if (accion.equals("create") == false) {
+            //Obtiene el parametro de Id del request y asigna el valor a la propiedad 
+            //Id de la instancia
+            resultadosseriea.setId(Integer.parseInt(Utilidad.getParameter(request, "id",
+                    "0")));
+        }
+        resultadosseriea.setEquipo1(Utilidad.getParameter(request, "Equipo1", ""));
+        if (accion.equals("index")) {
+            resultadosseriea.setTop_aux(Integer.parseInt(Utilidad.getParameter(request,
+                    "top_aux", "10")));
+            resultadosseriea.setTop_aux(resultadosseriea.getTop_aux() == 0 ? Integer.MAX_VALUE : resultadosseriea.getTop_aux());
+        }
+        resultadosseriea.setResultadoDeportes(Utilidad.getParameter(request, "ResultadoDeportes", ""));
+        if (accion.equals("index")) {
+            resultadosseriea.setTop_aux(Integer.parseInt(Utilidad.getParameter(request,
+                    "top_aux", "10")));
+            resultadosseriea.setTop_aux(resultadosseriea.getTop_aux() == 0 ? Integer.MAX_VALUE : resultadosseriea.getTop_aux());
+        }
+        resultadosseriea.setEquipo2(Utilidad.getParameter(request, "Equipo2", ""));
+        if (accion.equals("index")) {
+            resultadosseriea.setTop_aux(Integer.parseInt(Utilidad.getParameter(request,
+                    "top_aux", "10")));
+            resultadosseriea.setTop_aux(resultadosseriea.getTop_aux() == 0 ? Integer.MAX_VALUE : resultadosseriea.getTop_aux());
+        }
+        return resultadosseriea;
+    }
+
+    protected void doGetRequestIndex(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ResultadoSerieaServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ResultadoSerieaServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        try {
+            ResultadosSerieA resultadosseriea = new ResultadosSerieA();
+            resultadosseriea.setTop_aux(10);
+            ArrayList<ResultadosSerieA> resultadoseriea = ResultadoSerieADAL.buscar(resultadosseriea);
+            request.setAttribute("resultadosseriea", resultadosseriea);
+            request.setAttribute("top_aux", resultadosseriea.getTop_aux());
+            request.getRequestDispatcher("Views/ResultadosSerieA/index.jsp")
+                    .forward(request, response);
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+
+    }
+
+    protected void doPostRequestIndex(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            ResultadosSerieA resultadosseriea = obtenerResultadosSerieA(request);
+            ArrayList<ResultadosSerieA> resultadoseriea = ResultadoSerieADAL.buscar(resultadosseriea);
+            request.setAttribute("resultadosseriea", resultadosseriea);
+            request.setAttribute("top_aux", resultadosseriea.getTop_aux());
+            request.getRequestDispatcher("Views/ResultadosSerieA/index.jsp")
+                    .forward(request, response);
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+    protected void doGetRequestCreate(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("Views/ResultadosSerieA/create.jsp")
+                .forward(request, response);
+    }
+
+    protected void doPostRequestCreate(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            ResultadosSerieA resultadosseriea = obtenerResultadosSerieA(request);
+            int result = ResultadoSerieADAL.crear(resultadosseriea);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("Error al Guardar el Regisgtro", request, response);
+            }
+
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+    protected void requestObtenerPorId(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            ResultadosSerieA resultadosseriea = obtenerResultadosSerieA(request);
+            ResultadosSerieA resultadosseriea_result = ResultadoSerieADAL.obtenerPorId(resultadosseriea);
+            if (resultadosseriea_result.getId() > 0) {
+                request.setAttribute("resultadosseriea", resultadosseriea_result);
+            } else {
+                Utilidad.enviarError("El id: " + resultadosseriea.getId() + " no existe en la tabla resultadosseriea",
+                        request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+    protected void doGetRequestEdit(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/ResultadosSeriea/edit.jsp")
+                .forward(request, response);
+    }
+
+    protected void doPostRequestEdit(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            ResultadosSerieA resultadosseriea = obtenerResultadosSerieA(request);
+            int result = ResultadoSerieADAL.modificar(resultadosseriea);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("Error al Guardar el Regisgtro", request, response);
+            }
+
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+    protected void doGetRequestDetails(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/ResultadosSerieA/details.jsp")
+                .forward(request, response);
+    }
+
+    protected void doGetRequestDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/ResultadosSerieA/delete.jsp")
+                .forward(request, response);
+    }
+
+    protected void doPostRequestDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            ResultadosSerieA resultadosseriea = obtenerResultadosSerieA(request);
+            int result = ResultadoSerieADAL.eliminar(resultadosseriea);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("Error al Guardar el Regisgtro", request, response);
+            }
+
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
         }
     }
 
@@ -63,7 +200,36 @@ public class ResultadoSerieaServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        SessionUser.authorize(request, response, () -> {
+            String accion = Utilidad.getParameter(request,
+                    "accion", "index");
+            switch (accion) {
+                case "index":
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+                    break;
+                case "create":
+                    request.setAttribute("accion", accion);
+                    doGetRequestCreate(request, response);
+                    break;
+                case "edit":
+                    request.setAttribute("accion", accion);
+                    doGetRequestEdit(request, response);
+                    break;
+                case "delete":
+                    request.setAttribute("accion", accion);
+                    doGetRequestDelete(request, response);
+                    break;
+                case "details":
+                    request.setAttribute("accion", accion);
+                    doGetRequestDetails(request, response);
+                    break;
+                default:
+                    request.setAttribute("accion", accion);
+                    doGetRequestIndex(request, response);
+                    break;
+            }
+        });
     }
 
     /**
@@ -77,17 +243,32 @@ public class ResultadoSerieaServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //SessionUser.authorize(request, response, () -> {
+        String accion = Utilidad.getParameter(request,
+                "accion", "index");
+        switch (accion) {
+            case "index":
+                request.setAttribute("accion", accion);
+                doPostRequestIndex(request, response);
+                break;
+            case "create":
+
+                request.setAttribute("accion", accion);
+                doPostRequestCreate(request, response);
+                break;
+            case "edit":
+                request.setAttribute("accion", accion);
+                doPostRequestEdit(request, response);
+                break;
+            case "delete":
+                request.setAttribute("accion", accion);
+                doPostRequestDelete(request, response);
+                break;
+            default:
+                request.setAttribute("accion", accion);
+                doGetRequestIndex(request, response);
+                break;
+        }
+        //});
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
